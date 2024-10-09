8fa9358e9600ee5b43f868eb7ceacd0770d7b50e
Junnie2412 <dungchan321@gmail.com>
2024-10-09 15:56:50 +0700

    Blog_Update

--- KoiAuction/src/components/Blogs/Blogs.tsx
+++ KoiAuction/src/components/Blogs/Blogs.tsx
@@ -1,63 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Pagination from '../Pagination/Pagination'
-
-const BlogPost = ({ title, date, excerpt }: { title: string; date: string; excerpt: string }) => (
-  <article className='border border-gray-200 rounded-lg overflow-hidden'>
-    <img
-      src='https://firebasestorage.googleapis.com/v0/b/koiaution.appspot.com/o/HomePage%2FBlogKoi1.png?alt=media&token=1502433d-7db5-465c-8fa5-07592db8f1da'
-      alt='Koi fish in a pond'
-      className='w-full h-auto object-cover'
-    />
-    <div className='p-4'>
-      <h3 className='text-lg font-semibold mb-2'>{title}</h3>
-      <p className='text-sm text-gray-600 mb-2'>{date}</p>
-      <p className='mb-4'>{excerpt}</p>
-      <a href='#' className='text-red-600 hover:underline'>
-        Read More »
-      </a>
-    </div>
-  </article>
-)
+import { Blog } from '../../types/Blog.type'
 
 export default function Blogs() {
-  const blogPosts = [
-    // Example blog posts array
-    {
-      title: 'Japanese Koi Buying Trip - 2020',
-      date: 'April 6, 2020',
-      excerpt:
-        'Japanese Koi Buying Trip 2020 This year we heard that there might be a shortage of small koi available, so we made the decision to...'
-    },
-    {
-      title: 'Koi Pond Maintenance Tips',
-      date: 'March 10, 2021',
-      excerpt:
-        'Learn how to maintain your koi pond for optimal health and beauty. We share essential tips for keeping your pond clean and your koi happy.'
-    },
-    {
-      title: 'The Beauty of Koi Colors',
-      date: 'February 15, 2022',
-      excerpt: 'Explore the various colors and patterns of koi fish and learn how they affect their value and appeal.'
-    },
-    {
-      title: 'Feeding Your Koi: What You Need to Know',
-      date: 'January 5, 2023',
-      excerpt:
-        'Proper nutrition is vital for the health of your koi. Discover the best foods and feeding practices to ensure your koi thrive.'
-    },
-    {
-      title: 'The History of Koi Fish',
-      date: 'December 20, 2023',
-      excerpt: 'Dive into the rich history of koi fish, their origins, and their significance in various cultures.'
-    }
-    // You can add more posts here
-  ]
-
-  const postsPerPage = 2 // Adjust the number of posts per page
-  const totalPages = Math.ceil(blogPosts.length / postsPerPage)
+  const [blogPosts, setBlogPosts] = useState<Blog[]>([])
+  const [loading, setLoading] = useState(true)
+  const postsPerPage = 2
   const [currentPage, setCurrentPage] = useState(1)
 
-  // Calculate the posts to display on the current page
+  useEffect(() => {
+    const fetchBlogPosts = async () => {
+      try {
+        const response = await fetch('https://6706402ba0e04071d2260d4e.mockapi.io/koiauction/blogs')
+        const data = await response.json()
+        setBlogPosts(data)
+      } catch (error) {
+        console.error('Error fetching blog posts:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchBlogPosts()
+  }, [])
+
+  const totalPages = Math.ceil(blogPosts.length / postsPerPage)
   const indexOfLastPost = currentPage * postsPerPage
   const indexOfFirstPost = indexOfLastPost - postsPerPage
   const currentPosts = blogPosts.slice(indexOfFirstPost, indexOfLastPost)
@@ -66,6 +33,10 @@ export default function Blogs() {
     if (pageNumber > 0 && pageNumber <= totalPages) {
       setCurrentPage(pageNumber)
     }
+  }
+
+  if (loading) {
+    return <div>Loading...</div>
   }
 
   return (
@@ -94,8 +65,18 @@ export default function Blogs() {
           </p>
 
           <div className='grid md:grid-cols-2 gap-6 mb-6 lg:mt-20 mt-10'>
-            {currentPosts.map((post, index) => (
-              <BlogPost key={index} title={post.title} date={post.date} excerpt={post.excerpt} />
+            {currentPosts.map((post) => (
+              <article key={post.id} className='border border-gray-200 rounded-lg overflow-hidden'>
+                <img src={post.image} alt={post.title} className='w-full h-auto object-cover' />
+                <div className='p-4'>
+                  <h3 className='text-lg font-semibold mb-2'>{post.title}</h3>
+                  <p className='text-sm text-gray-600 mb-2'>{post.postedDate}</p>
+                  <p className='mb-4'>{post.content}</p>
+                  <a href='#' className='text-red-600 hover:underline'>
+                    Read More »
+                  </a>
+                </div>
+              </article>
             ))}
           </div>
 
